2897fc3733880a152bf729d4329880e2babeb87b
imamhossenbu <imam.cse9.bu@gmail.com>
2026-05-10 14:18:17 +0600

    add list

---
+++ list.js
@@ -0,0 +1,13 @@
+const client = require("./client");
+
+async function init() {
+  const result = await client.lpush("messages", "one", "two", "three");
+
+  console.log(result);
+
+  const data = await client.lrange("messages", 0, -1);
+
+  console.log(data);
+}
+
+init();
--- string.js
+++ string.js
@@ -1,9 +1,9 @@
 const client = require("./client");
 
 async function init() {
-//   await client.set("user:4", "Abul");
-//   await client.expire("user:4", 10);
-  const result = await client.get("user:4");
+  const result = await client.set("user:4", "rahim", "nx");
+  //   await client.expire("user:4", 10);
+  //   const result = await client.get("user:4");
   console.log(result);
 }
 
